945dc5529320a7e0a3917ae40c965f86c65e7b67
Joël Gähwiler <joel.gaehwiler@gmail.com>
2014-12-31 12:54:52 +0100

    added gulp based release tasks

---
+++ Gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var del = require('del');
+var zip = require('gulp-zip');
+
+gulp.task('clean', function(cb){
+  del(['./build'], cb);
+});
+
+gulp.task('copy', ['clean'], function(){
+  return gulp.src('./**/*')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('thin', ['copy'], function(cb){
+  del([
+    './build/update.sh',
+    './build/Gulpfile.js',
+    './build/package.json',
+    './build/node_modules'
+  ], cb);
+})
+
+gulp.task('compress', ['thin'], function () {
+  return gulp.src('build/*')
+    .pipe(zip('mqtt.zip'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['compress']);
