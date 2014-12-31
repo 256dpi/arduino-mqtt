437f151a760205804dd01909d2403eb35de078c9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2014-12-31 10:56:10 +0100

    removed spaces

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -4,23 +4,23 @@
 Timer::Timer() {  
   this->interval_end_ms = 0L;
 }
-    
+
 Timer::Timer(int ms) {
   countdown_ms(ms);   
 }
-    
+
 bool Timer::expired() {
   return (interval_end_ms > 0L) && (millis() >= interval_end_ms);
 }
-    
+
 void Timer::countdown_ms(unsigned long ms) {
   interval_end_ms = millis() + ms;
 }
-    
+
 void Timer::countdown(int seconds) {
   countdown_ms((unsigned long)seconds * 1000L);
 }
-    
+
 int Timer::left_ms() {
   return interval_end_ms - millis();
 }
