1d7784bb3e0fd9ace6e5c9c9ad02cf81efa20ddb
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-04-09 16:07:42 +0200

    fixed install script

--- src/YunMQTTInstall.cpp
+++ src/YunMQTTInstall.cpp
@@ -2,54 +2,35 @@
 
 #include <Process.h>
 
-void sendCommand(const char *name, const char * cmd, const char * param1, const char * param2) {
+void sendCommand(const char *name, const char * cmd) {
   Serial.print("=== ");
-  Serial.print(name);
-  Serial.println(" ===");
+  Serial.println(name);
+
+  Serial.print("--> running '");
+  Serial.print(cmd);
+  Serial.println("'");
 
   Process p;
   p.begin(cmd);
-  p.addParameter(param1);
-
-  if(param2 != NULL) {
-    p.addParameter(param2);
-  }
-
-  Serial.print("> running '");
-  Serial.print(cmd);
-  Serial.print(' ');
-  Serial.print(param1);
-
-  if(param2 != NULL) {
-    Serial.print(' ');
-    Serial.print(param2);
-  }
-
-  Serial.println("'");
-
-  p.run();
+  p.runShellCommand(cmd);
 
   while (p.available() > 0) {
     char c = p.read();
     Serial.print(c);
   }
 
-  Serial.flush();
-
-  Serial.println("> finished!");
-  Serial.println("");
+  Serial.println("--> finished!\n");
 }
 
 void YunMQTTInstall() {
-  Serial.println("Starting Installation!");
-  Serial.println("");
+  Serial.println("Starting Installation!\n");
 
-  sendCommand("Update", "opkg", "update", NULL);
-  sendCommand("Install 'easy_install'", "opkg", "install", "distribute");
-  sendCommand("Install python SSL support", "opkg", "install", "python-openssl");
-  sendCommand("Install 'pip'", "easy_install", "pip", NULL);
-  sendCommand("Install 'paho-mqtt'", "pip", "install", "paho-mqtt");
-  sendCommand("Download script", "wget", "https://raw.githubusercontent.com/256dpi/arduino-mqtt/yun-client/yun/client.py", NULL);
+  sendCommand("Update", "opkg update");
+  sendCommand("Install 'easy_install'", "opkg install distribute");
+  sendCommand("Install python SSL support", "opkg install python-openssl");
+  sendCommand("Install 'pip'", "easy_install pip");
+  sendCommand("Install 'paho-mqtt'", "pip install paho-mqtt");
+  sendCommand("Download script", "wget https://raw.githubusercontent.com/256dpi/arduino-mqtt/yun-client/yun/client.py --no-check-certificate -o /usr/client.py");
 
   Serial.println("Installation finished!");
 }
