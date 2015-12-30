4f9c00489dabd427c4fd84879a62c93174c49230
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-12-30 13:05:45 +0100

    added binary payload support

--- src/YunMQTTClient.cpp
+++ src/YunMQTTClient.cpp
@@ -101,11 +101,15 @@ void YunMQTTClient::publish(const char * topic, String payload) {
 }
 
 void YunMQTTClient::publish(const char * topic, const char * payload) {
+  this->publish(topic, (char*)payload, strlen(payload));
+}
+
+void YunMQTTClient::publish(const char * topic, char * payload, unsigned int length) {
   // send publish request
   this->process.print("p:");
   this->process.print(topic);
   this->process.print(':');
-  this->process.print(strlen(payload));
+  this->process.print(length);
   this->process.print(';');
   this->process.print(payload);
   this->process.print('\n');
