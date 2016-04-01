235f502878944fc8a79322cbd5a6ff0005393487
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-04-01 16:53:26 +0200

    check if client is connected before calling read() or write()

--- src/Network.cpp
+++ src/Network.cpp
@@ -10,16 +10,24 @@ int Network::connect(char* hostname, int port) {
 }
 
 int Network::read(unsigned char* buffer, int len, int timeout) {
-	if(this->client->available() > 0) {
-		this->client->setTimeout(timeout);
-		return this->client->readBytes(buffer, len);
-	} else {
-		return 0; // immediately return if there is nothing to read
-	}
+  if(!client->connected()) {
+    return -1; // return an error
+  }
+
+	if(this->client->available() == 0) {
+    return 0; // nothing to read
+  }
+
+  this->client->setTimeout(timeout);
+  return this->client->readBytes(buffer, len);
 }
     
 int Network::write(unsigned char* buffer, int len, int timeout) {
-  client->setTimeout(timeout);  
+  if(!client->connected()) {
+    return -1; // return an error
+  }
+
+  client->setTimeout(timeout);
   return client->write((uint8_t*)buffer, len);
 }
     
