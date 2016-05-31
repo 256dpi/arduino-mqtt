b7424d07573541d8f8371db5f2db50b7ba48900c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-05-31 12:22:58 +0200

    fixed a small regression

--- src/YunMQTTClient.cpp
+++ src/YunMQTTClient.cpp
@@ -121,7 +121,7 @@ void YunMQTTClient::publish(const char * topic, char * payload, unsigned int len
   this->process.print(topic);
   this->process.print(F(":"));
   this->process.print(length);
-  this->process.print(F("l"));
+  this->process.print(F(";"));
 
   for(int i=0; i<length; i++) {
     this->process.write(payload[i]);
