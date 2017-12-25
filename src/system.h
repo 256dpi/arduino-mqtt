3f9f89a65e320ca3ee9e06bfdf53fb0ded925488
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-12-25 11:55:14 +0100

    fmt

--- src/system.h
+++ src/system.h
@@ -8,13 +8,17 @@ extern "C" {
 #include "lwmqtt/lwmqtt.h"
 };
 
-typedef struct { uint32_t end; } lwmqtt_arduino_timer_t;
+typedef struct {
+  uint32_t end;
+} lwmqtt_arduino_timer_t;
 
 void lwmqtt_arduino_timer_set(void *ref, uint32_t timeout);
 
 int32_t lwmqtt_arduino_timer_get(void *ref);
 
-typedef struct { Client *client; } lwmqtt_arduino_network_t;
+typedef struct {
+  Client *client;
+} lwmqtt_arduino_network_t;
 
 lwmqtt_err_t lwmqtt_arduino_network_read(void *ref, uint8_t *buf, size_t len, size_t *read, uint32_t timeout);
 lwmqtt_err_t lwmqtt_arduino_network_write(void *ref, uint8_t *buf, size_t len, size_t *sent, uint32_t timeout);
