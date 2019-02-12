29cbdd35a248d19fdf5996e302165b12f7a0d90b
Bongjun Hur <bong@wiznet.io>
2019-02-12 18:34:07 +0900

    modify ethernet codes with new Arduino Ethernet library v2

--- examples/ArduinoEthernetShield/ArduinoEthernetShield.ino
+++ examples/ArduinoEthernetShield/ArduinoEthernetShield.ino
@@ -11,7 +11,11 @@
 #include <MQTT.h>
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-byte ip[] = {192, 168, 1, 177};  // <- change to match your network
+//byte ip[] = {192, 168, 1, 177};  // <- change to match your network
+IPAddress ip(192, 168, 1, 177);
+IPAddress myDns(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 EthernetClient net;
 MQTTClient client;
@@ -36,8 +40,57 @@ void messageReceived(String &topic, String &payload) {
 }
 
 void setup() {
+/*
   Serial.begin(115200);
   Ethernet.begin(mac, ip);
+*/
+  // for Arduino Ethernet V2 library
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+      while (!Serial) {
+          ; // wait for serial port to connect. Needed for native USB port only
+      }
+
+  // start the Ethernet connection:
+  Serial.println("Trying to get an IP address using DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // initialize the Ethernet device not using DHCP:
+    Ethernet.begin(mac, ip, myDns, gateway, subnet);
+  }
+
+  Ethernet.MACAddress(mac); // fill the MAC buffer
+  Serial.print("The MAC address is: ");
+  for (byte octet = 0; octet < 6; octet++) {
+    Serial.print(mac[octet], HEX);
+    if (octet < 5) {
+      Serial.print('-');
+    }
+  }
+  Serial.println();
+
+  // print your local IP address:
+  ip = Ethernet.localIP(); // update the IP address buffer
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 
   // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
   // You need to set the IP address directly.
