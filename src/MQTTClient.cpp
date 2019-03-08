e9b1c73b498e92e480014461a1096e365cb60146
Rushikesh Patel <rushikeshpatelc502@gmail.com>
2019-03-08 11:50:16 +0530

    error status moved before return

--- src/MQTTClient.cpp
+++ src/MQTTClient.cpp
@@ -260,8 +260,8 @@ bool MQTTClient::connect(const char clientId[], const char username[], const cha
   if (!skip) {
     int ret = this->netClient->connect(this->hostname, (uint16_t)this->port);
     if (ret <= 0) {
-      return false;
       this->_lastError = LWMQTT_NETWORK_FAILED_CONNECT;
+      return false;
     }
   }
 
