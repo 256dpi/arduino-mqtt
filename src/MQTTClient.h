d6d9e7d2adad81aa407fcb42f77a23efce8967da
Drzony <drzony@gmail.com>
2020-10-12 10:52:13 +0200

    prevent timer rollover

--- src/MQTTClient.h
+++ src/MQTTClient.h
@@ -12,7 +12,8 @@ extern "C" {
 typedef uint32_t (*MQTTClientClockSource)();
 
 typedef struct {
-  uint32_t end;
+  uint32_t start;
+  uint32_t timeout;
   MQTTClientClockSource millis;
 } lwmqtt_arduino_timer_t;
 
@@ -49,8 +50,8 @@ class MQTTClient {
   MQTTClientCallback callback;
 
   lwmqtt_arduino_network_t network = {nullptr};
-  lwmqtt_arduino_timer_t timer1 = {0, nullptr};
-  lwmqtt_arduino_timer_t timer2 = {0, nullptr};
+  lwmqtt_arduino_timer_t timer1 = {0, 0, nullptr};
+  lwmqtt_arduino_timer_t timer2 = {0, 0, nullptr};
   lwmqtt_client_t client = lwmqtt_client_t();
 
   bool _connected = false;
